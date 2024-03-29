22ccda76ff5ddef5119ae6930c7e42c8a83e5a8d
Saksham Chakradhar <sakshamchakradhar151@gmail.com>
2024-03-29 16:03:01 +0530

    new section update

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,6 +30,29 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== SWIPER PROJECTS ===============*/
 
+let swiperProjects = new Swiper(".projects_container", {
+    
+    loop: true,
+    spaceBetween:24,
+
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    pagination: {
+      el: ".swiper-pagination",
+    },
+
+    breakpoints: {
+        1200: {
+          slidesPerView: 2,
+          spaceBetween: -56,
+        },
+      },
+  });
+  
+
 
 /*=============== SWIPER TESTIMONIAL ===============*/
 
