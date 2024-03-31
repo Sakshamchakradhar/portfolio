b8c3a2b67a69c34e6fcbca3b41c28e84522cab01
Saksham Chakradhar <sakshamchakradhar151@gmail.com>
2024-03-31 17:39:18 +0530

    Few changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -54,28 +54,101 @@ let swiperProjects = new Swiper(".projects_container", {
   
 
 
-/*=============== Submit button animation ===============*/
-         
- const btn = document.querySelector("#btn");
- const btnText = document.querySelector("#btnText");
-         
- btn.onclick = () => {
- btnText.innerHTML = "Thanks";
- btn.classList.add("active");
- };
+/*=============== Submit button animation and Email JS ===============*/
 
+const btn = document.querySelector("#btn");
+const btnText = document.querySelector("#btnText");
 
-/*=============== SWIPER TESTIMONIAL ===============*/
+const contactForm = document.getElementById('contact-form'),
+  contactName = document.getElementById('contact-name'),
+  contactEmail = document.getElementById('contact-email'),
+  contactProject = document.getElementById('contact-project'),
+  contactMessage = document.getElementById('contact-message');
 
+const sendEmail = (e) => {
+  e.preventDefault();
 
-/*=============== EMAIL JS ===============*/
+  // Check if the field has a value
+  if (contactName.value === '' || contactEmail.value === '' || contactProject.value === '') {
+    // Add and remove color
+    contactMessage.classList.remove('color-blue');
+    contactMessage.classList.add('color-red');
 
+    // Show message
+    contactMessage.textContent = 'Write all the input field 📩';
+
+    // Reset button text and remove animation class
+    btnText.innerHTML = "Submit";
+    btn.classList.remove("active");
+  } else {
+    // serviceID - templateID - #form -publicKey
+    emailjs.sendForm('service_f7e4kfk', 'template_trk3wdk', '#contact-form', 'LAxcr6Ci_nfkkEWpL')
+      .then(() => {
+        // Show message and add color 
+        contactMessage.textContent = 'Message sent ✅';
+
+        // Remove message after five seconds
+        setTimeout(() => {
+          contactMessage.textContent = '';
+        }, 3000);
+
+        // Reset button text and remove animation class
+        btnText.innerHTML = "Submit";
+        btn.classList.remove("active");
+
+        // Clear input fields
+        contactName.value = '';
+        contactEmail.value = '';
+        contactProject.value = '';
+      })
+      .catch((error) => {
+        alert('OOPS! SOMETHING HAS FAILED....', error);
+
+        // Reset button text and remove animation class
+        btnText.innerHTML = "Submit";
+        btn.classList.remove("active");
+      });
+      
+    // Update button text to indicate sending
+    btnText.innerHTML = "Sending..."; 
+    btn.classList.add("active");
+  }
+}
+
+contactForm.addEventListener('submit', sendEmail);
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollDown = window.scrollY
+
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+		if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
 
 
 /*=============== SHOW SCROLL UP ===============*/ 
 
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						: scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 /*=============== DARK LIGHT THEME ===============*/ 
 
