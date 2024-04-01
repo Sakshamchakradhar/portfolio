e805601e87ecc75c83c2376a8fbdba1bf8f95022
Saksham Chakradhar <sakshamchakradhar151@gmail.com>
2024-04-01 22:39:49 +0530

    new changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -128,14 +128,14 @@ const scrollActive = () =>{
 		const sectionHeight = current.offsetHeight,
 			  sectionTop = current.offsetTop - 58,
 			  sectionId = current.getAttribute('id'),
-			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']');
 
 		if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
-			sectionsClass.classList.add('active-link')
+			sectionsClass.classList.add('active-link');
 		}else{
-			sectionsClass.classList.remove('active-link')
+			sectionsClass.classList.remove('active-link');
 		}                                                    
-	})
+	});
 }
 window.addEventListener('scroll', scrollActive)
 
@@ -150,8 +150,59 @@ const scrollUp = () =>{
 }
 window.addEventListener('scroll', scrollUp)
 
+
+/*=============== Resume download ===============*/ 
+
+document.getElementById('downloadButton').addEventListener('click', function() {
+  // Replace 'path/to/resume.pdf' with the actual path to your resume PDF file
+  var fileUrl = 'saksham-resume.pdf';
+
+  // Creating a temporary anchor element
+  var a = document.createElement('a');
+  a.href = fileUrl;
+
+  // Setting the file name
+  a.download = 'Saksham-Resume.pdf';
+
+  // Appending the anchor to the body and triggering the click event
+  document.body.appendChild(a);
+  a.click();
+
+  // Removing the anchor from the body
+  document.body.removeChild(a);
+});
+
+
 /*=============== DARK LIGHT THEME ===============*/ 
 
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'ri-sun-line'
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
